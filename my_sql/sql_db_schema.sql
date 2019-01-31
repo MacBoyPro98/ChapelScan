6ecae1cf5dc5cca3bbeb1d45597fc2dd7752b5c3
Nathan Teeter <skater501@me.com>
2019-01-31 13:45:43 -0700

    Deleted old files and added SQL schema

---
+++ my_sql/sql_db_schema.sql
@@ -0,0 +1,24 @@
+-- auto-generated definition
+create table scans
+(
+  id         int                                not null,
+  fName      varchar(255)                       null,
+  lName      varchar(255)                       null,
+  CREATED_AT datetime default CURRENT_TIMESTAMP not null,
+  UPDATED_AT datetime                           null,
+  PHOTO      longtext                           null
+);
+
+-- auto-generated definition
+create table students
+(
+  fName   varchar(255) not null,
+  lName   varchar(255) not null,
+  CARD_ID int          not null,
+  STU_ID  int          not null,
+  PHOTO   longtext     null,
+  constraint students_CARD_ID_uindex
+    unique (CARD_ID),
+  constraint students_STU_ID_uindex
+    unique (STU_ID)
+);
