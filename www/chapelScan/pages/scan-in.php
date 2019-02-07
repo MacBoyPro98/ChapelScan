4e8c662db453ddad31cfdba35ad25e23e5330674
Nathan Teeter <skater501@me.com>
2019-02-07 12:00:44 -0700

    Fixes to scan-in.php

--- www/chapelScan/pages/scan-in.php
+++ www/chapelScan/pages/scan-in.php
@@ -16,7 +16,7 @@ include_once $_SERVER["DOCUMENT_ROOT"]."/chapelScan/login/config.php";
 
 //$connect = mysqli_connect('localhost','root','','login');
 
-$results = mysqli_query($connect, "SELECT * FROM scans ORDER BY CREATED_AT ASC");
+$results = mysqli_query($connect, "SELECT * FROM scans ORDER BY CREATED_AT DESC");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -182,9 +182,9 @@ $results = mysqli_query($connect, "SELECT * FROM scans ORDER BY CREATED_AT ASC")
                                         if ($row['PHOTO'] == null) {
                                             echo "Photo Not Available";
                                         } else {?>
-                                            <img src="../function/get_pic.php?id=?">
-<!--                                            <img src="data:image/png;base64,--><?php
-//                                            echo $row['PHOTO'];
+<!--                                            <img src="../function/get_pic.php?id=?">-->
+                                            <img src="data:image/png;base64,<?php
+                                            echo $row['PHOTO'];
                                         } ?>">
                                     </td>
                                     <td><?php echo $row['CREATED_AT']; ?></td>
